906d63184d62323352fdcc48f22aa9aef518e2a2
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2026-05-05 13:03:31 +0200

    ✨ Add create task DTO

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
-import { UserResponseDto } from 'src/users/dto/user-response.dto';
 
 
 export class CreateTaskDto {
