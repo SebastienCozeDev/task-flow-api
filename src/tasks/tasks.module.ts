7922d3ff423141b86d86e6cf780768a7872422cc
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2026-05-05 12:04:32 +0200

    ✨ Add task entity

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesGuard } from "src/auth/roles.guard";
 import { TasksService } from "./tasks.service";
+import { Task } from "./entities/board-member.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Board])],
+    imports: [TypeOrmModule.forFeature([Task])],
     controllers: [],
     providers: [TasksService, RolesGuard],
     exports: [],
