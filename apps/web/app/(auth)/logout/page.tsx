7ff3b09d7ebedfb55017719e0aab407acd00a5ad
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2026-05-14 15:34:22 +0200

    ✨ Add logout route

---
+++ apps/web/app/(auth)/logout/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { logoutUser } from "@/lib/auth";
+
+export default function LoginPage() {
+  const router = useRouter();
+  logoutUser();
+  router.replace("/login");
+  return null;
+}
