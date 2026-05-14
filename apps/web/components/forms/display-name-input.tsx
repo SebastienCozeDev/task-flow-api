123980bd2624f274e297ac833aec098228c8f366
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2026-05-14 18:25:31 +0200

    🚸 Add SVG for all inputs

--- apps/web/components/forms/display-name-input.tsx
+++ apps/web/components/forms/display-name-input.tsx
@@ -12,11 +12,11 @@ export default function DisplayNameInput(data: DisplayNameInputData) {
     <>
       <label className="label">Display Name</label>
       <div className="input">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[1em] opacity-50">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-[1em] opacity-50">
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
           />
         </svg>
         <input type="text" className="grow" placeholder="Display Name" value={data.value} onChange={data.onChange} />
--- apps/web/components/forms/email-input.tsx
+++ apps/web/components/forms/email-input.tsx
@@ -11,7 +11,16 @@ export default function EmailInput(data: EmailInputData) {
   return (
     <>
       <label className="label">Email</label>
-      <input type="email" className="input" placeholder="Email" value={data.value} onChange={data.onChange} />
+      <div className="input">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[1em] opacity-50">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+          />
+        </svg>
+        <input type="email" className="grow" placeholder="Email" value={data.value} onChange={data.onChange} />
+      </div>
     </>
   );
 }
