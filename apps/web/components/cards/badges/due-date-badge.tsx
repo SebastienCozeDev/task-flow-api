aaa7172be98696fa14a075facd3d8d962455fb2d
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2026-05-15 18:14:01 +0200

    ✨ Add badges

---
+++ apps/web/components/cards/badges/due-date-badge.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { getTodayAsIsoDate } from "@/lib/utils";
+
+type DueDateBadgeData = {
+  date: string;
+}
+
+export default function DueDateBadge(data: DueDateBadgeData) {
+  const today = getTodayAsIsoDate();
+  const isDueNowOrLate = data.date <= today;
+
+  return (
+    <div className={`badge ${isDueNowOrLate ? "badge-error" : "badge-outline"}`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-[1.2em]"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+      {data.date}
+    </div>
+  );
+}
--- apps/web/components/cards/task-card.tsx
+++ apps/web/components/cards/task-card.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { TaskResponse } from "@/lib/api/tasks-api";
+import Link from "next/link";
+import MoreLinkBadge from "./badges/more-link-badge";
+import DueDateBadge from "./badges/due-date-badge";
 
 type TaskCardData = {
   task: TaskResponse
@@ -9,35 +12,41 @@ type TaskCardData = {
 
 export default function TaskCard(data: TaskCardData) {
   return (
-    <>
-      <div className="card bg-base-100 w-full shadow-sm mx-auto">
-        {
-          data.task.imageLink
-          ? (
-            <figure>
-              <img
-                src={data.task.imageLink}
-                alt="Presentation image" />
-            </figure>
-          )
-          : null
-        }
-        <div className="card-body">
-          <h2 className="card-title">
-            {data.task.title}
-            {
-              data.showState && data.task.state
-              ? <div className="badge badge-secondary">{data.task.state.toUpperCase()}</div>
-              : null
-            }
-          </h2>
-          <p>{data.task.description}</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
-          </div>
+    <div className="card bg-base-100 w-full shadow-sm mx-auto">
+      {
+        data.task.imageLink
+        ? (
+          <figure>
+            <img
+              src={data.task.imageLink}
+              alt="Presentation image" />
+          </figure>
+        )
+        : null
+      }
+      <div className="card-body">
+        <h2 className="card-title">
+          {data.task.title}
+          {
+            data.showState && data.task.state
+            ? <div className="badge badge-secondary">{data.task.state.toUpperCase()}</div>
+            : null
+          }
+        </h2>
+        <p>{data.task.description}</p>
+        <div className="card-actions justify-end">
+          {
+            data.task.dueDate
+            ? <DueDateBadge date={data.task.dueDate} />
+            : null
+          }
+          {
+            data.task.moreLink
+            ? <MoreLinkBadge href={data.task.moreLink} />
+            : null
+          }
         </div>
       </div>
-    </>
+    </div>
   );
 }
