142e30e62a242f9648409df23479363560c2ef29
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2026-05-15 16:26:09 +0200

    🐛 Fix description length bug

--- apps/api/src/tasks/dto/task.dto.ts
+++ apps/api/src/tasks/dto/task.dto.ts
@@ -1,5 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsDate, IsOptional, IsString } from 'class-validator';
+import { IsDate, IsOptional, IsString, MaxLength } from 'class-validator';
 import { Type } from 'class-transformer';
 
 
@@ -7,16 +7,19 @@ export class TaskDto {
     @ApiPropertyOptional({ example: 'My Super Description', description: 'The description of the task' })
     @IsOptional()
     @IsString()
+    @MaxLength(255)
     description?: string;
 
     @ApiPropertyOptional({ example: 'https://sebastien.cozedev.com/img/icon.png', description: 'The image link of the task' })
     @IsOptional()
     @IsString()
+    @MaxLength(255)
     imageLink?: string;
 
     @ApiPropertyOptional({ example: 'https://sebastien.cozedev.com/', description: 'The link of the task to get more detail' })
     @IsOptional()
     @IsString()
+    @MaxLength(255)
     moreLink?: string;
 
     @ApiPropertyOptional({ example: '2026-05-05', description: 'The due date of the task' })
